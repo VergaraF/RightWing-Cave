89e8d6cc0cb5b1eef7c3d11a23e5a714db9ed1c5
Fabian Vergara <vergarf@gmail.com>
2016-10-12 16:57:36 -0400

    Fixed bug.
    Fixed logic bug where my algorithm would crash or go into an infinite
    recursion causing stack over flow error. Now it doesn't crash what so
    ever.

--- RightWing-Cave/src/RightWingGame.java
+++ RightWing-Cave/src/RightWingGame.java
@@ -4,24 +4,22 @@
  */
 public class RightWingGame {
 	public static final int DEFAULT_VALUE = 0;
+	
 	public static void main(String[] args){
-		int[] play = {4, 8, 5, 2, 3, 5, 1, 6, 4, 0};
+		//[5] 8 2 3 1 5 0
+		int[] play = {5, 8, 2, 3, 1, 5, 0};
 		
-		System.out.println(solveGameRecursively(play, DEFAULT_VALUE, DEFAULT_VALUE));
+		System.out.println(solveGameRecursively(play, DEFAULT_VALUE, DEFAULT_VALUE, DEFAULT_VALUE));
 	}
 	
-	public static boolean solveGameRecursively(int[] game, int index, int oldValue){
-		
+	public static boolean solveGameRecursively(int[] game, int index, int oldValue, int oldIndex){
 		if (game[index] <= 0){
 			return true;
 		}
-		if (index + game[index] < game.length){
-			return solveGameRecursively(game, index + game[index], game[index]);
+		if (index + game[index] < game.length && (index + game[index] != oldIndex)){
+			return solveGameRecursively(game, index + game[index], game[index], index);
 		}else if (index - game[index] >= 0){	
-			if (oldValue == game[index]) {
-				return false;
-			}
-			return solveGameRecursively(game, index - game[index], game[index]);
+			return solveGameRecursively(game, index - game[index], game[index], index);
 		}
 		
 		return false;
